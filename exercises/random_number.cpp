505a3eeac8a1d82cde8c59f34ade9734d45e7775
Fabulousqueen <perronegiada2004@gmail.com>
2025-10-30 18:32:23 +0100

    feat: random_number.cpp implemented

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,13 @@
   Output:
   The random number is: 4
 */
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+
+  int x = rand();
+  cout << "The random number is: " << x << endl;
+
+  return 0;
+}
